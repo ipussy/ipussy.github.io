cd249242faf3c48694f2d5e83aa1cc14bf6dae21
Pusy Studio <pusy.studio@gmail.com>
2020-09-02 20:42:14 +0700

    Schedule

--- crawler.py
+++ crawler.py
@@ -172,10 +172,10 @@ def scheduleCrawler():
 
 
 
-schedule.every(2).hours.do(scheduleCrawler)
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+#schedule.every(2).hours.do(scheduleCrawler)
+#while True:
+#    schedule.run_pending()
+#    time.sleep(60)
 
 
 
